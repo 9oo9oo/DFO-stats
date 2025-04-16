0642df756f05f0ea8eb7eda7ccad45fb4732d92f
Jin Gwak <jinhyeokgwak@gmail.com>
2025-04-16 16:29:25 +1000

    minor change in server

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+app.use(express.static('../client/dist'));
 
 // Middleware
 app.use(express.json());
