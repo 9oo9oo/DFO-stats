6888b84a0e5c3a6d1f260c079cb2c76f427c7fe8
9oo9oo <jinhyeokgwak@gmail.com>
2025-04-30 12:38:24 +1000

    added script to automate data update & logging

---
+++ scripts/update_data.sh
@@ -0,0 +1,9 @@
+source ~/.bash_profile
+cd ~/DFO-stats/scripts
+
+node automationFetchCharacterIds.js
+node automationFetchEquipment.js
+node automationFetchAvatar.js
+node automationFetchCreature.js
+node automationFetchTalisman.js
+node automationFetchSkills.js
