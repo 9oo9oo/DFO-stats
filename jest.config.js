3cfc54cb14086bd388fd1fa1850d2b48363066ba
Jin Gwak <jinhyeokgwak@gmail.com>
2025-07-03 16:18:57 +1000

    setting up for frontend unit test

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,43 @@
 module.exports = {
   testEnvironment: 'node',
-  rootDir: 'src',
-  testMatch: ['**/__tests__/**/*.js', '**/*.spec.js'],
+
+  roots: [
+    '<rootDir>/src',         // backend
+    '<rootDir>/client/src',  // frontend
+  ],
+
+  moduleFileExtensions: ['js', 'json', 'vue'],
+
+  transform: {
+    '^.+\\.vue$': '@vue/vue3-jest',
+    '^.+\\.js$':  'babel-jest',
+  },
+
+  testMatch: [
+    '<rootDir>/src/**/__tests__/**/*.spec.js',
+    '<rootDir>/src/**/*.spec.js',
+    '<rootDir>/client/src/**/__tests__/**/*.spec.js',
+    '<rootDir>/client/src/**/*.spec.js',
+  ],
+
+  moduleNameMapper: {
+    '^@/(.*)$':         '<rootDir>/src/$1',
+    '^@client/(.*)$':   '<rootDir>/client/src/$1',
+  },
+
+  collectCoverage: true,
   collectCoverageFrom: [
-    '**/*.{js,jsx}', 
-    '!**/components/**', 
-    '!**/assets/**', 
-    '!**/config/**', 
-    '!server.js', 
-    '!**/coverage/**'],
-    reporters: [
+    'src/**/*.js',
+    '!src/server.js',
+    '!src/config/*.js',
+    'client/src/**/*.js',
+    'client/src/**/*.vue',
+    '!client/src/main.js',
+    '!client/src/config/*.js',
+    '!client/src/router/*.js',
+  ],
+
+  reporters: [
     "default",
     ["jest-junit", {
       outputDirectory: "test-results",
